0068c8294c80e92a8c89cf648315516f0b5becec
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2019-12-15 00:27:42 +0000

    Test on VM'

--- atari/main.py
+++ atari/main.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     env = gym.make('Breakout-ramNoFrameskip-v4')
     agent = Agent(rm, policy_net, target_net, optimizer, criterion, env, device)
     reward_hist = []
+    epsilon_hist = []
     if DYNAMIC_PLOT:
         plt.ion()
     for episode in range(N_EPISODES):
@@ -29,6 +30,7 @@ if __name__ == '__main__':
                                          update_target_net=(episode % TARGET_UPDATE == 0),
                                          save_nets=(SAVE_MODELS and episode == N_EPISODES - 1))
         reward_hist.append(total_reward)
+        epsilon_hist.append(epsilon)
         if VERBOSE:
             print('episode:', episode + 1, '- total reward:', total_reward)
         if DYNAMIC_PLOT:
@@ -38,6 +40,7 @@ if __name__ == '__main__':
             plt.xlabel('Episode')
             plt.ylabel('Total reward')
             plt.plot(reward_hist)
+            plt.plot(epsilon_hist)
         epsilon -= (epsilon > MINIMAL_EPSILON) * EPSILON_ANNEALING_STEP
     env.close()
     if DYNAMIC_PLOT:
@@ -49,4 +52,5 @@ if __name__ == '__main__':
         plt.xlabel('Episode')
         plt.ylabel('Total reward')
         plt.plot(reward_hist)
+        plt.plot(epsilon_hist)
         plt.savefig(PLOTS_DIR + FILE_SUFFIX + '.png')
