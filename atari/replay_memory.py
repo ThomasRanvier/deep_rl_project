4de648590a3a6533519fcf5df6e4b56cdf68a4fe
ThomasRanvier <ranvier.thomas.pro@gmail.com>
2019-12-22 21:38:34 +0100

    Modify rm to save lots of memory

--- atari/replay_memory.py
+++ atari/replay_memory.py
@@ -4,6 +4,7 @@ class ReplayMemory():
     def __init__(self, capacity):
         self._capacity = capacity
         self._memory = []
+        self._frames = []
 
     def push(self, transition):
         if len(self._memory) >= self._capacity:
@@ -12,6 +13,22 @@ class ReplayMemory():
 
     def sample(self, batch_size):
         return random.sample(self._memory, min(len(self._memory), batch_size))
+
+    def add_frame(self, frame):
+        f_idx = None
+        i = 0
+        for f in self._frames:
+            if (f == frame).all():
+                f_idx = i
+                break
+            i += 1
+        if f_idx is None:
+            f_idx = len(self._frames)
+            self._frames.append(frame)
+        return f_idx
+
+    def get_frame(self, f_idx):
+        return self._frames[f_idx]
 
     def __len__(self):
         return len(self._memory)
