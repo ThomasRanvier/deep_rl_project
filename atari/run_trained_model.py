9cc79d0e68a78a04bc185ee44128202c4b2cea5f
ThomasRanvier <ranvier.thomas.pro@gmail.com>
2020-01-05 00:35:01 +0100

    Clean up

--- atari/run_trained_model.py
+++ atari/run_trained_model.py
@@ -18,13 +18,27 @@ SAVE_GIF = True
 # 44
 #MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c1550000.pt'
 # 85 régulier !!!!!!!!!!!!!!!
-MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c1600000.pt'
+#MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c1600000.pt'
 # 43
 #MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c1750000.pt'
+# 35
+#MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c1950000.pt'
+# 64
+#MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c2000000.pt'
+# 39
+#MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c2050000.pt'
+# 46
+#MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c2450000.pt'
+# 46
+#MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c3100000.pt'
 
 
 # New lr00001
-#MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr00001/policy_net_lr1e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c1550000.pt'
+# 23
+#MODEL_PATH = '../../backup_models/norm_dbl_duel_g99_bs32_lr00001/policy_net_lr1e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c2500000.pt'
+
+# No reward clip bs32 g99
+MODEL_PATH = '../../backup_models/norewardclip_norm_dbl_duel_g99_bs32_lr0000625/policy_net_lr6.25e-05_bs32_tu2500_it4500000_g0.99_ed0.2777777777777778_c1350000.pt'
 
 def run_episode(net, env):
     env.reset()
